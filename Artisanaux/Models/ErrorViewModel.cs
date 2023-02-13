e75985fb7c22b8afa2bc93ca838a54a1d51779d2
Ayoub Senhaji <senraja0000@gmail.com>
2023-02-13 14:17:24 +0100

    firstCommit

---
+++ Artisanaux/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Artisanaux.Models
+{
+    public class ErrorViewModel
+    {
+        public string ? RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
